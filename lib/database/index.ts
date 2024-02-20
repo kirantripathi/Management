ef97e3f74fec10eaac9b8530c20c689d4900bf20
Sagar Sigdel <sigdelsagar31@gmail.com>
2024-02-20 23:01:08 +1000

    try to find error position

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -8,7 +8,7 @@ let cached = (global as any).mongoose || {conn:null,promise:null};
 export const connectToDatabase = async () => {
 
  
-    console.log("db called");
+  
     if(cached.conn) return cached.conn;
 
     if(!MONGODB_URI) throw new Error("MONGODB_URI is missing")
@@ -16,9 +16,9 @@ export const connectToDatabase = async () => {
     try {
         cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
             dbName: 'evently',
-            bufferCommands: true // Adjust as needed
+            bufferCommands: false // Adjust as needed
         });
-
+console.log("try to make db connection")
         cached.conn = await cached.promise;
         console.log('Connected to MongoDB');
         return cached.conn;
